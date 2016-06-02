2c91c1df2407683bd60ccf0b1bb7df12bba1b31d
nick-nie <ziyng.nie@gmail.com>
2016-06-02 20:36:15 +0800

    add extentreport listener

--- src/test/java/base/TestCase.java
+++ src/test/java/base/TestCase.java
@@ -4,12 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import util.TestContext;
 import webdriver.WebDriverFactory;
+import webdriver.listener.ExtentReporterNG;
 
 /**
  * Created by Administrator on 2016/5/29 0029.
  */
+@Listeners(ExtentReporterNG.class)
 public class TestCase {
     protected WebDriver driver;
     public final static String BASEURL = "http://demosite.center/wordpress/wp-login.php";
